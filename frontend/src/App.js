787a3ad2e6c9a5b9d9b4ea3276a18bb639ecb128
Anish Maharana <anishjkhm@gmail.com>
2023-08-17 12:31:52 +0530

    ecommerce

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, SignupPage, HomePage} from "./Routes.js";
+import { LoginPage, SignupPage, HomePage, ProductsPage, BestSellingPage, EventsPage, FAQPage} from "./Routes.js";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Store from "./redux/store";
@@ -19,7 +19,10 @@ const App = () => {
         <Route path="/" element={<HomePage />} /> 
         <Route path="/Login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
-
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/best-selling" element={<BestSellingPage />} />
+        <Route path="/events" element={<EventsPage/>} />
+        <Route path="/faq" element={<FAQPage/>} />
       </Routes>
       <ToastContainer
       position="top-right"
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,17 @@
 import LoginPage from "../src/pages/Login";
 import SignupPage from "../src/pages/SignupPage";
 import HomePage from "../src/pages/HomePage";
+import ProductsPage from "../src/pages/ProductsPage";
+import BestSellingPage from "../src/pages/BestSellingPage";
+import EventsPage from "../src/pages/EventsPage";
+import FAQPage from "../src/pages/FAQPage";
 
 export {
     LoginPage,
     SignupPage,
     HomePage,
+    ProductsPage,
+    BestSellingPage,
+    EventsPage,
+    FAQPage,
 };
